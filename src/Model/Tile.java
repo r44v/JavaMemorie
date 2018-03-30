1f985e442c451f9e00cf98fbf0f2b67aa7d5f6c6
r44v <rs.raav@gmail.com>
2018-03-30 22:44:41 +0200

    Added Tile Class

---
+++ src/Model/Tile.java
@@ -0,0 +1,43 @@
+package Model;
+
+public class Tile {
+	
+	// elke tile krijgt een getal toegekend;
+	private int number;
+	
+	//true als het plaatje ‘omgedraaid’ is, en dus getoond moet worden;
+	private boolean selected;
+	
+	//true als het plaatje geraden is (en dus ook getoond moet worden);
+	private boolean guessed;
+	
+	//Some Getters/Setters
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+
+	public boolean isGuessed() {
+		return guessed;
+	}
+
+	public void setGuessed(boolean guessed) {
+		this.guessed = guessed;
+	}
+	
+	//Observer pattern
+	public void update() {
+		//
+	}
+}
